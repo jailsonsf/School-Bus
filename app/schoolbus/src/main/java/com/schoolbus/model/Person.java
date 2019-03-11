3e870d143365c36464a402e4dfc8c938c31fd0e1
jailsonsf <jailsonsoares002@gmail.com>
2019-03-11 18:04:22 -0300

    Remove import unused :rocket:

--- app/schoolbus/src/main/java/com/schoolbus/model/Person.java
+++ app/schoolbus/src/main/java/com/schoolbus/model/Person.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Person {
 

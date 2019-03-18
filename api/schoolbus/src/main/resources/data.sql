57dea6f86f7d4aa4b2f5eca2d8392ea4bd53beee
jailsonsf <jailsonsoares002@gmail.com>
2019-03-18 18:37:18 -0300

    Update DB :rocket:

--- api/schoolbus/src/main/resources/data.sql
+++ api/schoolbus/src/main/resources/data.sql
@@ -1,1 +1,4 @@
 insert into student(id, address, age, cpf, name, rg, class_time, course) values(1, 'Rua x', 20, '111', 'jailson', '123', 'tarde', 'computação')
+insert into attendant(id, address, age, cpf, name, rg) values (2, 'Rua y', 28, '123', 'zequinha', '1234')
+insert into driver(id, address, age, cpf, name, rg, cnh) values(3, 'Rua z', 39, '1234', 'joaozinho', '12345', '123456')
+insert into educational_institution(id, name, cnpj, address) values(4, 'escola tu que sabe', '1234567', 'Rua z')

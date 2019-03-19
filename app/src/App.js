13068b50dad9aac73042387b4c4b46477dfd4cd0
jailsonsf <jailsonsoares002@gmail.com>
2019-03-19 09:39:35 -0300

    Add app ReactJs :rocket:

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Header from './components/Header/index'
+
+const App = () => (
+  <div className="App">
+    <Header />  
+  </div>
+)
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1 align="center">Hello world</h1>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
